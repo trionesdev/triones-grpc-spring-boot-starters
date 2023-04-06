c081cebf740a2e51c41c5ed001cc093af4393bf7
bane.shi <fengxiaotx@163.com>
2023-04-06 20:46:17 +0800

    [dev]

--- moensun-grpc-server-spring-boot-starter/src/main/java/com/moensun/grpc/server/GrpcServerRunner.java
+++ moensun-grpc-server-spring-boot-starter/src/main/java/com/moensun/grpc/server/GrpcServerRunner.java
@@ -1,9 +1,18 @@
 package com.moensun.grpc.server;
 
+import com.moensun.grpc.server.autoconfigure.GrpcServerProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.CommandLineRunner;
 
+@Slf4j
 public class GrpcServerRunner implements CommandLineRunner, DisposableBean {
+    private final GrpcServerProperties  grpcServerProperties;
+
+    public GrpcServerRunner(GrpcServerProperties grpcServerProperties) {
+        this.grpcServerProperties = grpcServerProperties;
+    }
+
     @Override
     public void destroy() throws Exception {
 
